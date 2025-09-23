daab88bd55eca12928c15cb0acc3a62d3bf7e1ba
Anderson <118529145+andberna28@users.noreply.github.com>
2025-09-23 19:36:39 +0000

    Finalização da atividade da Toalha e feita a atividade Enxugar

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -7,12 +7,31 @@ class Towel:
     def __str__(self):
         return f"color: {self.color}, tam: {self.size}, umi: {self.wetness}"
 
+    def dry(self, amount: int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.getMaxWetness():
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness()
 
-toalha = Towel("red", "G")
-toalha2 = Towel("black", "P")
+    def wringOut(self) -> None:
+        self.wetness = 0
+        print("A toalha foi torcida, umidade zerada")
 
-print(toalha.color)
-toalha.color = "white"
-print(toalha.color)
-print(toalha.size)
-print(toalha.wetness)
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
+
+    def isDry(self) -> bool:
+        return self.wetness == 0
+
+
+toalha = Towel("vermelha", "G")
+toalha.dry(10)
+toalha.dry(40)
+toalha.wringOut()
+print(toalha)
